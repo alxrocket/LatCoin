8554c2fa41b48fd5fbe461ebed5054ea9b40be5d
Evan Duffield <evan@dash.org>
2016-12-09 10:04:03 -0700

    Flip IsSynced check (seems reversed?)

--- src/masternode-payments.cpp
+++ src/masternode-payments.cpp
@@ -78,7 +78,7 @@ bool IsBlockValueValid(const CBlock& block, int nBlockHeight, CAmount blockRewar
 
     LogPrint("gobject", "block.vtx[0].GetValueOut() %lld <= nSuperblockMaxValue %lld\n", block.vtx[0].GetValueOut(), nSuperblockMaxValue);
 
-    if(!masternodeSync.IsSynced()) {
+    if(masternodeSync.IsSynced()) {
         // not enough data but at least it must NOT exceed superblock max value
         if(CSuperblock::IsValidBlockHeight(nBlockHeight)) {
             if(fDebug) LogPrintf("IsBlockPayeeValid -- WARNING: Client not synced, checking superblock max bounds only\n");
